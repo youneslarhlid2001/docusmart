794b5e4cb3053986e4fa43152f1c84b6960ec5f0
WilliamMollet <williammdev@gmail.com>
2026-03-19 17:04:33 +0100

    Correction Untrusted Host

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
